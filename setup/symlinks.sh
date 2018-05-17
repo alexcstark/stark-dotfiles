47204fead3ea0732316fca01ab4c8271a116676b
Alex Stark <alexcstark@gmail.com>
2018-05-17 10:53:30 -0700

    fix

--- setup/symlinks.sh
+++ setup/symlinks.sh
@@ -1,10 +1,7 @@
 ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/dotfiles/zshrc ~/.zshrc
-ln -s ~/dotfiles/ssh ~/.ssh
 ln -s ~/dotfiles/oh-my-zsh ~/.oh-my-zsh
-ln -s ~/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/dotfiles/tern-config ~/.tern-config
-ln -s ~/dotfiles/editorconfig ~/.editorconfig
+# Uncomment this if for stark git config - which should only be fore me?
+# ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/vscode ~/.vscode
-ln -s ~/dotfiles/lint/eslintrc.yml ~/.eslintrc.yml
 ln -s ~/dotfiles/.jupyter ~/.jupyter
